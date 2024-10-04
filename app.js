433b350df688fa53da4b4fab57f6caa09e88de4d
Ahmet Beyhan <ahmetrasimbayhan@gmail.com>
2024-10-04 12:20:22 +0300

    Listener Events

---
+++ app.js
@@ -0,0 +1,19 @@
+const filterInput = document.getElementById("filter");
+const todoForm = document.getElementById("todo-form");
+
+todoForm.addEventListener("submit",submitForm);
+function submitForm(e){
+    console.log("Submit Event");
+}
+// filterInput.addEventListener("focus",function(e){
+//     console.log(e);
+//     console.log(e.type);
+//     console.log(e.target);
+//     console.log(e.target.placeholder);
+//     console.log(e.target.className);
+//    // console.log("Naber");
+
+// });
+// filterInput.onfocus = function(){
+//     console.log("naber");
+// }
