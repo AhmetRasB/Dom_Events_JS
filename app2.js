3fdfe9c246147b826e6ca3d684d278ae02118de7
Ahmet Beyhan <ahmetrasimbayhan@gmail.com>
2024-10-07 19:46:29 +0300

    keydown

--- app2.js
+++ app2.js
@@ -5,8 +5,9 @@
 //     console.log(e.key);
 // }
 
-// document.addEventListener("keydown",run);
-// function run(e){
-//    // console.log(e.which); ASCII out
-//     console.log(e.key);
-// }
+document.addEventListener("keydown",run);
+function run(e){
+   // console.log(e.which); ASCII out
+    console.log(e.key);
+    //
+}
