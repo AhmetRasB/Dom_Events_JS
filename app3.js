a69397c91bc9d830e2ce9e77b1eadb702ea80a95
Ahmet Beyhan <ahmetrasimbayhan@gmail.com>
2024-10-07 20:39:07 +0300

    mouse up

--- app3.js
+++ app3.js
@@ -1,7 +1,12 @@
 const cardBody = document.querySelectorAll(".card-body")[1];
 const title = document.querySelector("#tasks-title");
 
-title.addEventListener("mousedown",run);
+// title.addEventListener("mousedown",run);
+// function run(e){
+//     console.log(e.type);
+// }
+
+title.addEventListener("mouseup",run);
 function run(e){
     console.log(e.type);
 }
